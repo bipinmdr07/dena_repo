8a39f19453a9cae8547c5dc3054928ec79a18e64
Takehiro Mouri <takehiromouri@gmail.com>
2015-12-18 07:44:32 +0000

    configured travis.yml

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -5,9 +5,7 @@ module StaticPagesHelper
 		items << ['HTML/CSS', "Craft beautiful front-end designs with HTML and CSS."]
 		items << ['JavaScript/jQuery', "Make your websites interactive with JavaScript and jQuery."]
 		items << ['Git/GitHub', "Learn what version control is and how you can use Git to collaborate with other awesome developers."]
-		# rubocop:disable Metrics/LineLength
 		items << ['Team Communication', "Pair up with another developer to work on a project together. We teach you how to use tools effectively for project management."]
-		# rubocop:enable Metrics/LineLength
 		items << ['Algorithms', "Learning algorithms are essential in your path to becoming a coder and will boost your logical thinking skills."]
   end
 end
