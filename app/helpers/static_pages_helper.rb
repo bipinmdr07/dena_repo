7ee3f67766bc73cb3db71c8ddc7f6fbabea390f2
Takehiro Mouri <takehiromouri@gmail.com>
2015-12-18 05:42:35 +0000

    Added landing page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+	def skills
+		items = []
+		items << ['Ruby on Rails', "Learn how to create dynamic apps with Ruby on Rails."]
+		items << ['HTML/CSS', "Craft beautiful front-end designs with HTML and CSS."]
+		items << ['JavaScript/jQuery', "Make your websites interactive with JavaScript and jQuery."]
+		items << ['Git/GitHub', "Learn what version control is and how you can use Git to collaborate with other awesome developers."]
+		items << ['Team Communication', "Pair up with another developer to work on a project together. We teach you how to use tools effectively for project management."]
+		items << ['Algorithms', "Learning algorithms are essential in your path to becoming a coder and will boost your logical thinking skills."]
+	end
 end
