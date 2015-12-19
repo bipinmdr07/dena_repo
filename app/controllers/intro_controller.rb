8aacff3730616ab3e55cdc1deaaabbbe04cbb0d1
Takehiro Mouri <takehiromouri@gmail.com>
2015-12-19 08:31:15 +0000

    Added intro lesson 2

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -7,7 +7,8 @@ class IntroController < ApplicationController
 
 	def show
 		@lesson = params[:id]
-    # @next_lesson = @lesson + 1 unless @lesson == 33
+    @next_lesson = @lesson.to_i + 1 unless @lesson == 33
+    @lesson == 1 ? @prev_lesson = 0 : @prev_lesson = @lesson.to_i - 1
 	end
 	
 end
