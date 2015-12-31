1d80919ba3b6f9c31a2c2e6ec8a6faaef7441ca7
Takehiro Mouri <takehiromouri@gmail.com>
2015-12-31 07:53:33 +0000

    send mail on reply

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,6 +1,8 @@
 class RepliesController < ApplicationController
 	def create
 		parent_comment.replies.create(reply_params)
+		UserMailer.new_reply(Forum.find(params[:forum_id]), parent_comment, User.find(parent_comment.user_id).email).deliver
+		UserMailer.new_reply(Forum.find(params[:forum_id]), parent_comment, "techrisecoding@gmail.com").deliver
 		redirect_to :back
 	end
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,25 @@ class UserMailer < ApplicationMailer
 		@post = post
 		mail(to: "techrisecoding@gmail.com", subject: 'New Question')
 	end
+
+	def new_submission(forum, post)
+		@forum = forum
+		@post = post
+		mail(to: "techrisecoding@gmail.com", subject: 'New Submission')
+	end
+
+	def new_reply(forum, post, email)
+		@forum = forum
+		@post = post
+		mail(to: email, subject: 'New Reply')
+	end
+
+	def new_submission_reply(forum, post)
+		recipients = ["techrisecoding@gmail.com", User.find(post.user_id).email]
+		@forum = forum
+		@post = post
+		mail(to: recipients, subject: 'New Reply')
+	end
+
+
 end
