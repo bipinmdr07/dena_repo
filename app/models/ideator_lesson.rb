78a9b2978772384c686c28ce046f619bc812e0e9
Takehiro Mouri <takehiromouri@gmail.com>
2016-01-20 04:04:32 +0000

    add lesson 12

--- app/models/ideator_lesson.rb
+++ app/models/ideator_lesson.rb
@@ -17,14 +17,15 @@ class IdeatorLesson < ActiveRecord::Base
 				[5, "Installing Bootstrap"],
 				[6, "Self Directed Lesson - Adding a Navbar"],
 				[7, "Styling the Navbar"],
-				[8, "Adding a Footer"],
-				[9, "Creating a Model"]
+				[8, "Adding a Footer"]
 			]
 		],
 		["Playing with the Database",
 			[
+				[9, "Creating a Model"],
 				[10, "Adding an Idea"],
-				[11, "Styling our Index Page"]
+				[11, "Styling our Index Page"],
+				[12, "Creating a Form"]
 			]
 		]
 	]
