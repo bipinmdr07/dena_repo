00e656119a30cf3a696f464cc01bc805d9c4c39d
Takehiro Mouri <takehiromouri@gmail.com>
2016-02-04 16:51:39 +0000

    remove unnnecessary js and css

--- app/controllers/intro_controller.rb
+++ app/controllers/intro_controller.rb
@@ -1,14 +1,3 @@
 class IntroController < LessonsController
-	# before_action :authenticate_user!
 
-	# def index
-	# 	@lessons = Intro::LESSONS
-	# end
-
-	# def show
-	# 	@lesson = params[:id]
- #    @next_lesson = @lesson.to_i + 1 unless @lesson == 33
- #    @lesson == 1 ? @prev_lesson = 0 : @prev_lesson = @lesson.to_i - 1
-	# end
-	
 end
