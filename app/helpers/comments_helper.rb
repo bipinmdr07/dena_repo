44d39926c0cba9d66c51556581824e01ce3998a1
Takehiro Mouri <takehiromouri@gmail.com>
2016-02-07 10:10:08 +0000

    fix questions page

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,7 +1,4 @@
 module CommentsHelper
-	def back_to_lesson
-		'/' + Forum.find(@comment.forum_id).name.downcase + '/' + @comment.lesson.to_s
-	end
 
 	def comment_owner(comment)
 		User.find(comment.user_id).name
--- app/helpers/submission_comments_helper.rb
+++ app/helpers/submission_comments_helper.rb
@@ -1,9 +1,4 @@
 module SubmissionCommentsHelper
-	
-	def submission_back_to_lesson
-		@submission_comment = SubmissionComment.find(params[:id])
-		'/html_css/' + @submission_comment.lesson.to_s
-	end
 
 	def submission_comment_owner(comment)
 		User.find(comment.user_id).name
