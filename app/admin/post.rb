9264e17c0bf8c36a4936f1303235790085f12238
Take Mouri <takehiromouri@gmail.com>
2016-06-09 18:29:51 +0900

    fix active admin

--- app/admin/post.rb
+++ app/admin/post.rb
@@ -1,6 +1,14 @@
 ActiveAdmin.register Post do
   permit_params :user_id, :title, :body, :featured_image
 
+  controller do
+    def find_resource
+      scoped_collection.friendly.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      scoped_collection.find(params[:id])
+    end
+  end
+
   form do |f|
     f.inputs 'Blog Post' do
       f.input :title
