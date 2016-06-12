05beeaf48867dd726c5dddc59cf96734bcbad97e
Take Mouri <takehiromouri@gmail.com>
2016-06-12 22:38:39 +0900

    change forms

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -2,4 +2,8 @@ module LessonsHelper
   def create_flashcard
     content_tag(:button, '<i class="fa fa-plus-circle" aria-hidden="true"></i> Create Flashcard'.html_safe, class: 'trigger btn btn-success')
   end
+
+  def standard_git_workflow
+    content_tag(:p, 'Make sure to go through the standard git workflow.')
+  end
 end
