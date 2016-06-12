c68ff5f63cd2a612ea0d5a12f1a1a83e6263ab64
Take Mouri <takehiromouri@gmail.com>
2016-06-12 11:46:11 +0900

    changes

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,2 +1,5 @@
 module LessonsHelper
+  def create_flashcard
+    content_tag(:button, '<i class="fa fa-plus-circle" aria-hidden="true"></i> Create Flashcard'.html_safe, class: 'trigger btn btn-success')
+  end
 end
