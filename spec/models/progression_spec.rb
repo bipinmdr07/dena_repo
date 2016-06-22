1ea81c2260cffc0574665dd25cc5648b3149acbe
Take Mouri <takehiromouri@gmail.com>
2016-06-22 12:34:34 +0900

    add spec skeleton

--- spec/models/progression_spec.rb
+++ spec/models/progression_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe Progression, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "shouldn't allow duplicate progression items" do
+  end
 end
