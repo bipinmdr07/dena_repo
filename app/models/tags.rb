7114dc8a797e6aae4b0048641f728295e1a87523
Take Mouri <takehiromouri@gmail.com>
2016-06-25 22:42:39 +0900

    add instapost lessons to tag

--- app/models/tags.rb
+++ app/models/tags.rb
@@ -64,6 +64,41 @@ class Tags < ActiveRecord::Base
       [20, "Adding Pagination"] => %w(),
       [21, "Last Few Tweaks"] => %w(),
       [22, "Congratulations!"] => %w()
+    },
+    "instapost_lessons" => {
+      [1, "Setting up your Web Dev Environment"] => %w(),
+      [2, "Setting Up Git"] => %w(),
+      [3, "Creating Wireframes"] => %w(),
+      [4, "Self-Directed Lesson: Installing Bootstrap"] => %w()
+      [5, "Installing Devise"] => %w(),
+      [6, "Creating the User Model"] => %w(),
+      [7, "Installing Carrierwave"] => %w(),
+      [8, "Integrating Amazon S3 with Carrierwave"] => %w(),
+      [9, "Creating the models and controllers"] => %w(),
+      [10, "Setting up the Routes"] => %w(),
+      [11, "Adding Log in/Log Out/Sign Up Links"] => %w(),
+      [12, "Setting Up User Forms"] => %w(),
+      [13, "Creating Posts"] => %w(),
+      [14, "Writing the Post Backend"] => %w(),
+      [15, "Displaying the Posts"] => %w(),
+      [16, "Updating Posts"] => %w(),
+      [17, "Using filters"] => %w(),
+      [18, "Deleting Posts"] => %w(),
+      [19, "Displaying Individual Posts"] => %w(),
+      [20, "Creating the models and controllers"] => %w(),
+      [21, "Self-Directed Lesson: Setting up Database Relations"] => %w(),
+      [22, "Setting up routes"] => %w(),
+      [23, "Posting Comments"] => %w(),
+      [24, "Creating the Comments Form"] => %w(),
+      [25, "Self-Directed Lesson: Displaying the comments"] => %w(),
+      [26, "Self-Directed Lesson: Deleting comments"] => %w(),
+      [27, "Deleting Comments Part 2"] => %w(),
+      [28, "Displaying user info"] => %w(),
+      [29, "Setting the basic styling"] => %w(),
+      [30, "Self-Directed Lesson: Styling the navbar"] => %w(),
+      [31, "Self-Directed Lesson: Styling Posts"] => %w(),
+      [32, "Self-Directed Lesson: Styling the Profile Page"] => %w(),
+      [33, "Final Touches"] => %w()
     }
   }
 
