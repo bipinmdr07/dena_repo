81f17aa4497b629b59f2e6c745c013b86ffb9810
Take Mouri <takehiromouri@gmail.com>
2016-07-01 12:21:47 +0900

    add rspec and new layout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627025204) do
+ActiveRecord::Schema.define(version: 20160701021258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -197,6 +197,11 @@ ActiveRecord::Schema.define(version: 20160627025204) do
     t.integer  "owner_id"
     t.text     "uri_cache"
     t.string   "simplified_type",        default: "file"
+  end
+
+  create_table "rspec_blog_lessons", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ruby_core_lessons", force: :cascade do |t|
---
+++ spec/factories/rspec_blog_lessons.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :rspec_blog_lesson do
+    
+  end
+end
