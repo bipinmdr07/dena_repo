61edfa81d6f79c9a44e1d6ca587e885128fbc59e
Take Mouri <takehiromouri@gmail.com>
2016-07-01 13:46:34 +0900

    fix bug

--- app/models/progression.rb
+++ app/models/progression.rb
@@ -4,8 +4,6 @@ class Progression < ActiveRecord::Base
 
   belongs_to :user
 
-  validates_uniqueness_of :lesson_id, scope: :course_name
-
   after_destroy :delete_tag_activity
 
   scope :today, -> { where("created_at >= ?", Time.zone.now.beginning_of_day) }
