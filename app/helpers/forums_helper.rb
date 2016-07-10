6a32b62cfc61834fc0033e360bc1c697d3a64776
Take Mouri <takehiromouri@gmail.com>
2016-07-10 13:58:16 +0900

    fix bug

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -7,7 +7,7 @@ module ForumsHelper
         lesson_name = Tags::LESSONS[forum.name.underscore].keys[comment.lesson - 1][1]
         link_to '< ' + lesson_name, "/" + forum.name.underscore + "/" + comment.lesson.to_s
       else
-        lesson_name = Tags::LESSONS[forum.name.underscore].keys[params[:lesson] - 1][1]
+        lesson_name = Tags::LESSONS[forum.name.underscore].keys[params[:lesson].to_i - 1][1]
         link_to '< ' + lesson_name, "/" + forum.name.underscore + "/" + params[:lesson].to_s
       end
     elsif params[:submission_id]
@@ -17,7 +17,7 @@ module ForumsHelper
         lesson_name = Tags::LESSONS[submission.name.underscore].keys[submission_comment.lesson - 1][1]
         link_to 'Back to ' + lesson_name, "/" + submission.name.underscore + "/" + submission_comment.lesson.to_s
       else
-        lesson_name = Tags::LESSONS[submission.name.underscore].keys[params[:lesson] - 1][1]
+        lesson_name = Tags::LESSONS[submission.name.underscore].keys[params[:lesson].to_i - 1][1]
         link_to 'Back to ' + lesson_name, "/" + submission.name.underscore + "/" + params[:lesson].to_s
       end
     end
