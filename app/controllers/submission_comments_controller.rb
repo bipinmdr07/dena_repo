9d0f74ae39ee02565ad53c79ed1bc4f506e6cc80
Take Mouri <takehiromouri@gmail.com>
2016-07-13 14:53:02 +0900

    fix submission view

--- app/controllers/submission_comments_controller.rb
+++ app/controllers/submission_comments_controller.rb
@@ -7,11 +7,18 @@ class SubmissionCommentsController < ApplicationController
 
 	def create
 		@submission_comment = current_user.submission_comments.create(comment_params)
-		user = User.find(@submission_comment.user_id)
 
-		UserMailer.new_submission(Submission.find(params[:submission_id]), @submission_comment).deliver
-		Slack.chat_postMessage(text: 'New submission by ' + user.name + '! View it <' + submission_submission_comment_url(@submission_comment.submission_id, @submission_comment) + '|here>.', username: 'TECHRISE Bot', channel: "#forum_questions", icon_emoji: ":smile_cat:")
-		redirect_to submission_submission_comment_path(@submission_comment.submission_id, @submission_comment)
+		if @submission_comment.valid?
+			user = User.find(@submission_comment.user_id)
+			UserMailer.new_submission(Submission.find(params[:submission_id]), @submission_comment).deliver
+			Slack.chat_postMessage(text: 'New submission by ' + user.name + '! View it <' + submission_submission_comment_url(@submission_comment.submission_id, @submission_comment) + '|here>.', username: 'TECHRISE Bot', channel: "#forum_questions", icon_emoji: ":smile_cat:")
+			redirect_to submission_submission_comment_path(@submission_comment.submission_id, @submission_comment)
+		else
+			flash[:alert] = "Invalid, please try again."
+			redirect_to new_submission_submission_comment_path(
+				submission_id: comment_params[:submission_id], 
+				lesson: comment_params[:lesson])
+		end
 	end
 
 	def show
