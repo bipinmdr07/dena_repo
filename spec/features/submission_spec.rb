bcddad255301cd653fae970065a2dd46a1b1f24a
Take Mouri <takehiromouri@gmail.com>
2016-07-21 23:55:39 +0545

    edit spec

--- spec/features/submission_spec.rb
+++ spec/features/submission_spec.rb
@@ -52,8 +52,29 @@ feature "Submission links" do
       approved: true,
       title: "Submission by Another User")
     visit '/html_css_lessons/4'
-    expect(page).to have_content "Submission by Another User"
+    expect(page).to have_content "#{submission_by_another_user.title}"
     expect(page).to have_content "My Submission"
     expect(page).to_not have_content "Submit Your Work"
+  end
+
+  scenario "User is an admin" do
+    @user.update(admin: true)
+
+    another_user = FactoryGirl.create(:user)
+    another_user_2 = FactoryGirl.create(:user)
+    submission_by_another_user = FactoryGirl.create(:submission, 
+      lesson_id: 4, 
+      user_id: another_user.id, 
+      approved: true,
+      title: "Submission by Another User")
+    submission_by_another_user = FactoryGirl.create(:submission, 
+      lesson_id: 4, 
+      user_id: another_user_2.id, 
+      approved: true,
+      title: "Submission by Another User 2")
+    visit '/html_css_lessons/4'
+    expect(page).to have_content "Submission by Another User"
+    expect(page).to have_content "Submission by Another User 2"
+    expect(page).to have_content "2 students including #{another_user.name} #{another_user_2.name} have passed this assignment."
   end
 end
