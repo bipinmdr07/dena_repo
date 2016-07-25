34251f22a6b29d454e76741ae508f831da0f7bbb
Take Mouri <takehiromouri@gmail.com>
2016-07-25 10:01:09 +0545

    fix bug

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -3,11 +3,9 @@ class CommunityController < ApplicationController
   
   def index
     if params[:query].present?
-      @questions = Question.search(params[:query]).page(params[:page]).records.order(:created_at)
+      @questions = Question.search(params[:query]).page(params[:page]).records
     else
       @questions = Question.includes(:replies).order("created_at DESC").paginate(page: params[:page], per_page: 10)
     end
-    # @questions = Question.includes(:replies).order("created_at DESC").paginate(page: params[:page], per_page: 15)
-    # @questions = Question.search(params)
   end
 end
