cc7a6a89457bcf63916a1591c80eb6f4c8ce3081
Take Mouri <takehiromouri@gmail.com>
2016-07-31 03:47:02 +0545

    updates

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,7 +1,8 @@
 ActiveAdmin.register User do
   permit_params :email, :intro_access, :html_css_access, :ruby_fundamentals_access, :ideator_access, 
   :ruby_core_access, :instapost_access, :github_access, :javascript_access, :admin, :command_line_access,
-  :rspec_blog_access, :mvc_access, :mentor
+  :rspec_blog_access, :mvc_access, :mentor, :start_date, :graduation_date, :remaining_mentor_sessions, :mentor_id, 
+  :facebook_handle, :twitter_handle, :github_handle, :personal_website
 
   index do
     column :name
@@ -22,6 +23,14 @@ ActiveAdmin.register User do
     column :rspec_blog_access
     column :mvc_access
     column :mentor
+    column :start_date
+    column :graduation_date
+    column :remaining_mentor_sessions
+    column :mentor_id
+    column :facebook_handle
+    column :twitter_handle
+    column :github_handle
+    column :personal_website
     actions
   end
 
@@ -42,6 +51,14 @@ ActiveAdmin.register User do
       f.input :rspec_blog_access
       f.input :mvc_access
       f.input :mentor
+      f.input :start_date, :as => :datepicker
+      f.input :graduation_date, :as => :datepicker
+      f.input :remaining_mentor_sessions
+      f.input :mentor_id, as: :select, collection: User.where(mentor: true).map{|u| ["#{u.name}", u.id]}
+      f.input :facebook_handle
+      f.input :twitter_handle
+      f.input :github_handle
+      f.input :personal_website
     end
     f.actions
   end
