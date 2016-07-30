69f1a7f7de7ee5757f7665dcc4fe82956b31ecdd
Take Mouri <takehiromouri@gmail.com>
2016-07-31 03:16:47 +0545

    add text to mentor sessions

--- app/controllers/student_sessions_controller.rb
+++ app/controllers/student_sessions_controller.rb
@@ -3,7 +3,7 @@ class StudentSessionsController < ApplicationController
 
   def index
     @mentor_sessions = current_user.mentor_sessions.order("created_at DESC")
-    @mentor = User.find(current_user.mentor_id)
+    @mentor = User.find(current_user.mentor_id) if current_user.mentor_id
   end
 
   def new
