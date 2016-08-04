2f37951004e85ad47782a73ce4204b26aaebf83d
Take Mouri <takehiromouri@gmail.com>
2016-08-04 16:19:10 +0545

    add upvotes

---
+++ app/controllers/question_downvotes_controller.rb
@@ -0,0 +1,15 @@
+class QuestionDownvotesController < ApplicationController
+  before_action :authenticate_user!
+
+  respond_to :js, :html, :json
+
+  def create
+    @question = Question.find(params[:id])
+    @question.downvote_from current_user
+
+    respond_to do |format|
+      format.html {}
+      format.js {}
+    end
+  end
+end
