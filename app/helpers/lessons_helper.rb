5c2a3b26876cc8de72ca58893c1f73243101f8f0
aadeshere <aadeshere1@gmail.com>
2016-08-05 14:52:44 +0545

    added feature to see which lesson the mentee is working on

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -1,4 +1,7 @@
 module LessonsHelper
+  def get_lesson_name(course_name, lesson_id)
+    Tags::LESSONS[course_name.underscore + 's'].keys[lesson_id.to_i - 1][1]
+  end
   def back_to_lesson
     if params[:id]
       forum = controller_name.classify.constantize.find(params[:id])
@@ -11,14 +14,14 @@ module LessonsHelper
       link_to ('<i class="fa fa-caret-left" aria-hidden="true"></i> ' + lesson_name).html_safe, "/" + course_name + "/" + params[:lesson_id].to_s, class: 'btn btn-back btn-default'
     end
   end
-  
+
   def create_flashcard
     content_tag(:button, '<i class="fa fa-plus-circle" aria-hidden="true"></i> Create Flashcard'.html_safe, class: 'trigger btn btn-flashcard')
   end
 
   def standard_git_workflow
-    content_tag(:p, 
-      '<i class="fa fa-github" aria-hidden="true"></i> Make sure to go through the <a href="#" data-toggle="modal" data-target="#git_workflow">standard git workflow</a>.'.html_safe, 
+    content_tag(:p,
+      '<i class="fa fa-github" aria-hidden="true"></i> Make sure to go through the <a href="#" data-toggle="modal" data-target="#git_workflow">standard git workflow</a>.'.html_safe,
       class: 'standard_git_workflow')
   end
 end
