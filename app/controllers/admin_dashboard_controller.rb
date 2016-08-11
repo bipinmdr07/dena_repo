eaf72e5703ac58cd810c703dc36d755b45d8a359
Take Mouri <takehiromouri@gmail.com>
2016-08-11 16:52:51 +0545

    add admin dashboard

--- app/controllers/admin_dashboard_controller.rb
+++ app/controllers/admin_dashboard_controller.rb
@@ -3,7 +3,22 @@ class AdminDashboardController < ApplicationController
   before_action :authenticate_admin!
 
   def index
-    @users = User.all
+    admitted = User.admitted
+    active_prework_students = User.active_prework_students
+    signed_up_this_week = User.signed_up_this_week
+    signed_up_last_week = User.signed_up_last_week
+    signed_up_this_month = User.signed_up_this_month
+
+    @stat_hash = {
+      admitted => "Admitted Students",
+      active_prework_students => "Active Prework Students",
+      signed_up_this_week => "signed up this week",
+      signed_up_last_week => "signed up last week",
+      signed_up_this_month => "signed up this month"
+    }
+
+    @mentor_sessions = MentorSession.includes(:student_session).all.paginate(page: params[:page], per_page: 5)
+
   end
 
   private
@@ -11,6 +26,6 @@ class AdminDashboardController < ApplicationController
   def authenticate_admin!
     return if current_user && current_user.admin
     flash[:alert] = "Unauthorized :("
-    redirect_to :back
+    redirect_to dashboard_path
   end
 end
