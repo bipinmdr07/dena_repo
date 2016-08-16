becfaf5ca94a6ab35456c4d829d66ed6c65bf5d7
Take Mouri <takehiromouri@gmail.com>
2016-08-16 08:34:25 +0545

    change tags.rb

--- app/models/tags.rb
+++ app/models/tags.rb
@@ -51,7 +51,8 @@ class Tags < ActiveRecord::Base
     },
     "ruby_core_lessons" => {
       [1, "Practice, Practice, Practice"] => %w(ruby),
-      [2, "Codewars"] => %w(ruby)
+      [2, "Coding Challenge: Check Palindrome"] => %w(ruby),
+      [3, "Coding Challenge: String Compressor"] => %w(ruby)
     },
     "ideator_lessons" => {
       [1, "Setting up your Web Dev Environment"] => %w(),
