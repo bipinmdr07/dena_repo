2a15605983c064de8d6af9ee848fbb80e94f0dae
Take Mouri <takehiromouri@gmail.com>
2016-08-30 21:06:45 +0545

    refactor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,69 +45,71 @@ $(window).on('scroll load', function() {
 });
 
 /* summernote editor */
-  $(function() {
-    var delete_episode_image, ready, upload_episode_image;
-    upload_episode_image = function(that, file) {
-      var data;
-      data = new FormData;
-      data.append('image[photo]', file);
-      return $.ajax({
-        data: data,
-        type: 'POST',
-        url: '/images',
-        cache: false,
-        contentType: false,
-        processData: false,
-        success: function(data) {
-          var img;
-          img = document.createElement('IMG');
-          img.src = data.url;
-          img.setAttribute('id', data.image_id);
-          return $(that).summernote('insertNode', img);
+$(function() {
+  var delete_episode_image, ready, upload_episode_image;
+  upload_episode_image = function(that, file) {
+    var data;
+    data = new FormData;
+    data.append('image[photo]', file);
+    return $.ajax({
+      data: data,
+      type: 'POST',
+      url: '/images',
+      cache: false,
+      contentType: false,
+      processData: false,
+      success: function(data) {
+        var img;
+        img = document.createElement('IMG');
+        img.src = data.url;
+        img.setAttribute('id', data.image_id);
+        return $(that).summernote('insertNode', img);
+      }
+    });
+  };
+  delete_episode_image = function(file_id) {
+    return $.ajax({
+      type: 'DELETE',
+      url: "/images/" + file_id,
+      cache: false,
+      contentType: false,
+      processData: false
+    });
+  };
+  ready = function() {
+    return $('[data-provider="summernote"]').each(function() {
+      return $(this).summernote({
+        height: 250,
+        toolbar: [['style', ['style']], ['font', ['bold', 'italic', 'underline', 'clear']], ['fontname', ['fontname']], ['color', ['color']], ['para', ['ul', 'ol', 'paragraph']], ['height', ['height']], ['table', ['table']], ['insert', ['link', 'picture', 'hr']], ['view', ['fullscreen', 'codeview']]],
+        prettifyHtml: false,
+        codemirror: {
+          lineNumbers: true,
+          tabSize: 2,
+          theme: "railscasts"
+        },
+        callbacks: {
+          onImageUpload: function(files) {
+            var img;
+            return img = upload_episode_image(this, files[0]);
+          },
+          onMediaDelete: function(target, editor, editable) {
+            var image_id;
+            image_id = target[0].id;
+            if (!!image_id) {
+              delete_episode_image(image_id);
+            }
+            return target.remove();
+          }
         }
       });
-    };
-    delete_episode_image = function(file_id) {
-      return $.ajax({
-        type: 'DELETE',
-        url: "/images/" + file_id,
-        cache: false,
-        contentType: false,
-        processData: false
-      });
-    };
-    ready = function() {
-      return $('[data-provider="summernote"]').each(function() {
-        return $(this).summernote({
-          height: 250,
-          toolbar: [['style', ['style']], ['font', ['bold', 'italic', 'underline', 'clear']], ['fontname', ['fontname']], ['color', ['color']], ['para', ['ul', 'ol', 'paragraph']], ['height', ['height']], ['table', ['table']], ['insert', ['link', 'picture', 'hr']], ['view', ['fullscreen', 'codeview']]],
-          prettifyHtml: false,
-          codemirror: {
-            lineNumbers: true,
-            tabSize: 2,
-            theme: "railscasts"
-          },
-          callbacks: {
-            onImageUpload: function(files) {
-              var img;
-              return img = upload_episode_image(this, files[0]);
-            },
-            onMediaDelete: function(target, editor, editable) {
-              var image_id;
-              image_id = target[0].id;
-              if (!!image_id) {
-                delete_episode_image(image_id);
-              }
-              return target.remove();
-            }
-          }
-        });
-      });
-    };
-    $(document).ready(ready);
+    });
+  };
+
+  $(document).ready(ready);
     return $(document).on('turbolinks:load', ready);
   });
+
   $(document).ready(function() {
-      $('#summernote').summernote();
-    });
+    $('#summernote').summernote();
+  });
 /*  summernote */
