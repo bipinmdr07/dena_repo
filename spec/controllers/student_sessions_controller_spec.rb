ff0a6c64c350aefbaf6387ae064ac0f004814405
aadeshere <aadeshere1@gmail.com>
2016-09-01 17:00:09 +0545

    test valid attribute to edit student session

--- spec/controllers/student_sessions_controller_spec.rb
+++ spec/controllers/student_sessions_controller_spec.rb
@@ -3,11 +3,11 @@ require 'rails_helper'
 RSpec.describe StudentSessionsController, type: :controller do
   let(:mentor) { FactoryGirl.create(:mentor) }
   let(:user) { FactoryGirl.create(:user, mentor_id: mentor.id) }
-  
+
   before :each do
     sign_in user
   end
-  
+
   describe "GET #index" do
     context "when user is signed in" do
       it "should render index" do
@@ -32,7 +32,7 @@ RSpec.describe StudentSessionsController, type: :controller do
           post :create, student_session: FactoryGirl.attributes_for(:student_session, mentor_session_id: mentor_session.id)
         }.to change(StudentSession, :count).by(1)
       end
-    end 
+    end
 
     context "when attributes are invalid" do
       it "shouldn't create a new mentor session" do
@@ -41,6 +41,20 @@ RSpec.describe StudentSessionsController, type: :controller do
           post :create, student_session: FactoryGirl.attributes_for(:invalid_student_session, mentor_session_id: mentor_session.id)
         }.to change(StudentSession, :count).by(0)
       end
-    end 
+    end
   end
+
+    describe "PATCH #update" do
+      context "when attributes are valid" do
+        it "should update the mentor logs" do
+          mentor_session = FactoryGirl.create(:mentor_session, user_id: user.id, mentor_id: mentor.id)
+          student_session = FactoryGirl.create(:student_session, user_id: user.id, mentor_id: mentor.id, mentor_session_id: mentor_session.id)
+
+          patch :update, student_session: FactoryGirl.attributes_for(:student_session, mentor_session_id: mentor_session.id, public_details: "It was awesome session"), id: student_session.id
+          student_session.reload
+
+          expect(student_session.public_details).to eq("It was awesome session")
+        end
+      end
+    end
 end
