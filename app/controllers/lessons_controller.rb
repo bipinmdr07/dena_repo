595665f990518e6f0f8705e05e5c828e299ce0be
Take Mouri <takehiromouri@gmail.com>
2016-09-06 21:41:34 +0545

    refactor lessons controller

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -13,32 +13,25 @@ class LessonsController < ApplicationController
     
     @current_course_title = course_title
     @current_course_link = course_link
-
     @current_course_lessons = current_course_lessons
-    @current_lesson_id = params[:id]
-    @current_course_length = current_controller.constantize.lesson_length
-    
-    
+    @current_course_length = current_controller.constantize.lesson_length  
     @current_course_name = current_controller + "s"
+    @current_lesson_id = params[:id]    
 
     @current_lesson_questions = Question.where(course_name: current_controller, lesson_id: @current_lesson_id)
                          .order("created_at DESC")
                          .paginate(page: params[:page], per_page: 5)
 
-    @paginated_submissions = Submission.where(course_name: current_controller, lesson_id: @current_lesson_id)
-                                       .order("created_at DESC")
-                                       .paginate(page: params[:page], per_page: 5)
+    @current_lesson_submissions = Submission.where(course_name: current_controller, lesson_id: @current_lesson_id)
+                                            .order("created_at DESC")
 
-    @all_submissions = Submission.where(course_name: current_controller, lesson_id: @current_lesson_id)
-                                 .order("created_at DESC")
+    @paginated_submissions = @current_lesson_submissions.paginate(page: params[:page], per_page: 5)                                 
 
-    @submissions = @all_submissions.approved
+    @current_lesson_approved_submissions = @current_lesson_submissions.includes(:user).approved
 
     @submission_by_current_user = current_user.submissions.find_by(lesson_id: @current_lesson_id, 
                                                                    course_name: current_controller)
     
-    @submission_user_ids = @submissions.pluck(:user_id).uniq.first(10)
-
     @next_lesson = @current_lesson_id.to_i + 1    
 
     if @current_lesson_id == 1 
