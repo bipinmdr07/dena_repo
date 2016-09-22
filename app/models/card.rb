0325b603c2872956b194cf3a8e36aafe79bc95d4
Take Mouri <takehiromouri@gmail.com>
2016-09-23 01:53:57 +0545

    extract card interval logic into a decorator

--- app/models/card.rb
+++ app/models/card.rb
@@ -30,81 +30,12 @@ class Card < ActiveRecord::Base
   end                      
 
   def update_code_syntax
-    new_question = code_syntax(question)
-    new_answer = code_syntax(answer)
-    self.question = new_question
-    self.answer = new_answer
+    syntax_builder = SyntaxBuilder.new(self)
+    self.question = syntax_builder.question
+    self.answer = syntax_builder.answer
   end  
 
-  # need to extract logic below to another object
   def update_interval!(quality_response)
-    @prev_ef = calculated_ef.nil? ? 0 : calculated_ef
-    @prev_interval = calculated_interval.nil? ? 0 : calculated_interval
-    @quality_response = quality_response
-
-    @calculated_interval = nil
-    @calculated_ef = nil
-    @repetition_date = nil
-
-    #if quality_response is below 3 start repetition from the begining, but without changing easiness_factor
-    if @quality_response < 3
-
-      @calculated_ef = @prev_ef
-      @calculated_interval = 0
-    else
-      calculate_easiness_factor
-      calculate_interval
-    end
-
-    calculate_date
-    update(calculated_interval: @calculated_interval, calculated_ef: @calculated_ef, repetition_date: @repetition_date)
-  end
-
-  def interval
-    @calculated_interval
-  end
-
-  def easiness_factor
-    @calculated_ef
-  end
-
-  def next_repetition_date
-    @repetition_date
-  end
-
-  private
-
-  # need to extract logic below to another object
-  def code_syntax(text)
-    language = text.match(/\`(.*?)\`/) # returns `ruby`
-    return text if language.nil?
-    text = CGI.escapeHTML(text)
-    text.gsub!(Regexp.new("#{language[0]}"), "<pre><code class='#{language[1]}'>")
-    text << "</code></pre>" if text.gsub!(/\`end\`/, "</code></pre>").nil?
-    return text
-  end
-  
-  # need to extract logic below to another object
-  def calculate_interval
-    if @prev_interval == 0
-      @calculated_interval = 1
-    elsif @prev_interval == 1
-      @calculated_interval = 6
-    else
-      @calculated_interval = (@prev_interval*@prev_ef).to_i
-    end
-  end
-
-  # need to extract logic below to another object
-  def calculate_easiness_factor
-    @calculated_ef = @prev_ef+(0.1-(5-@quality_response)*(0.08+(5-@quality_response)*0.02))
-    if @calculated_ef < 1.3
-      @calculated_ef = 1.3
-    end
-    @calculated_ef
-  end
-  
-  def calculate_date
-    @repetition_date = Date.today + @calculated_interval
+    IntervalUpdater.new(self, quality_response).update_card!
   end
 end
