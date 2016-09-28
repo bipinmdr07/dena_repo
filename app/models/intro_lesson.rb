0989096f9cd8082642b439c24b3d5928b406b44a
Take Mouri <takehiromouri@gmail.com>
2016-09-28 19:15:59 +0545

    refactor intro

--- app/models/intro_lesson.rb
+++ app/models/intro_lesson.rb
@@ -5,15 +5,17 @@ class IntroLesson < Lesson
 
 	PERIOD = "PREWORK"
 
-	LESSONS = [
-		["Introduction to TECHRISE", 
-			[
-				[1, "Welcome to TECHRISE"],
-				[2, "Why use the TECHRISE Flashcard System?"],
-				[3, "How to Embed Code with TECHRISE Flashcards"],
-				[4, "Getting Started"]
-			]
-		]
+	chapter_1 = Chapter.new("Introduction to TECHRISE")
+
+	CHAPTERS = [
+		chapter_1
+	]
+
+	LESSONS = [		
+		Lesson.new(1, "Welcome to TECHRISE", chapter_1),
+		Lesson.new(2, "Why use the TECHRISE Flashcard System?", chapter_1),
+		Lesson.new(3, "How to Embed Code with TECHRISE Flashcards", chapter_1),
+		Lesson.new(4, "Getting Started", chapter_1)
 	]
 
 
