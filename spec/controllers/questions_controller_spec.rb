7e1eb1b0bfb5a267b65fd3285d18d43fed1dc6a2
aadeshere <aadeshere1@gmail.com>
2016-09-30 21:44:47 +0545

    refactor tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,23 +3,31 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe "POST #create" do
     context "when the attributes are valid" do
-      before :each do
-        sign_in FactoryGirl.create(:user)
-      end
 
       it "creates a new question" do
+        user = FactoryGirl.create(:user)
+
+        sign_in user
+
         expect {
           post :create, question: FactoryGirl.attributes_for(:question)
         }.to change(Question, :count).by(1)
       end
 
       it "redirects the user to the created question page" do
+        user = FactoryGirl.create(:user)
+
+        sign_in user
         post :create, question: FactoryGirl.attributes_for(:question)
 
         expect(response).to redirect_to question_path(Question.last.id)
       end
 
       it "sends an email to the user" do
+        user = FactoryGirl.create(:user)
+
+        sign_in user
+
         expect {
           post :create, question: FactoryGirl.attributes_for(:question)
         }.to change { ActionMailer::Base.deliveries.count }.by(1)
@@ -27,17 +35,21 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     context "when the attributes are invalid" do
-      before :each do
-        sign_in FactoryGirl.create(:user)
-      end
 
       it "doesn't create a new question" do
+        user = FactoryGirl.create(:user)
+
+        sign_in user
+
         expect {
           post :create, question: FactoryGirl.attributes_for(:invalid_question)
         }.to change(Question, :count).by(0)
       end
 
       it "redirects the user to the new question page" do
+        user = FactoryGirl.create(:user)
+
+        sign_in user
         post :create, question: FactoryGirl.attributes_for(:invalid_question)
 
         expect(response).to redirect_to new_question_path(course_name: 'HtmlCssLesson', lesson_id: 1)
@@ -45,23 +57,31 @@ RSpec.describe QuestionsController, type: :controller do
     end
 
     context "when user is a mentor" do
-      before :each do
-        sign_in FactoryGirl.create(:user, mentor: true)
-      end
 
       it "creates a new question" do
+        mentor = FactoryGirl.create(:user, mentor: true)
+
+        sign_in mentor
+
         expect {
           post :create, question: FactoryGirl.attributes_for(:question)
         }.to change(Question, :count).by(1)
       end
 
       it "redirects the user to the created question page" do
+        mentor = FactoryGirl.create(:user, mentor: true)
+
+        sign_in mentor
         post :create, question: FactoryGirl.attributes_for(:question)
 
         expect(response).to redirect_to question_path(Question.last.id)
       end
 
       it "sends an email to the user" do
+        mentor = FactoryGirl.create(:user, mentor: true)
+
+        sign_in mentor
+
         expect {
           post :create, question: FactoryGirl.attributes_for(:question)
         }.to change { ActionMailer::Base.deliveries.count }.by(1)
@@ -70,15 +90,13 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe "GET #edit" do
-    let(:user) { FactoryGirl.create(:user) }
-    before :each do
-      sign_in user
-    end
 
     context "when the user is the person who created the post" do
       it "should render the edit template" do
+        user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: user.id)
 
+        sign_in user
         get :edit, id: question.id
 
         expect(response).to render_template :edit
@@ -87,8 +105,10 @@ RSpec.describe QuestionsController, type: :controller do
 
     context "when the post is a mentor_post" do
       it "should render the edit template" do
+        user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:mentor_post, user_id: user.id)
 
+        sign_in user
         get :edit, id: question.id
 
         expect(response).to render_template :edit
@@ -97,9 +117,11 @@ RSpec.describe QuestionsController, type: :controller do
 
     context "when the user isn't the person who created the post" do
       it "should redirect to the question path" do
+        user = FactoryGirl.create(:user)
         another_user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: another_user.id)
 
+        sign_in user
         get :edit, id: question.id
 
         expect(response).to redirect_to question_path(question)
@@ -116,8 +138,10 @@ RSpec.describe QuestionsController, type: :controller do
     context "when the user is person who created the post" do
       context "when the attributes are valid" do
         it "updates the post" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:question, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: "New Title!")
           question.reload
 
@@ -125,8 +149,10 @@ RSpec.describe QuestionsController, type: :controller do
         end
 
         it "redirects them to the question url" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:question, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: "New Title!")
 
           expect(response).to redirect_to question_path(question.id)
@@ -135,8 +161,10 @@ RSpec.describe QuestionsController, type: :controller do
 
       context "when the attributes are invalid" do
         it "doesn't update the post" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:question, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: nil, content: "New content")
           question.reload
 
@@ -144,8 +172,10 @@ RSpec.describe QuestionsController, type: :controller do
         end
 
         it "renders the edit page" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:question, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: nil, content: "New content")
 
           expect(response).to render_template :edit
@@ -156,8 +186,10 @@ RSpec.describe QuestionsController, type: :controller do
     context "when the post is a mentor_post" do
       context "when the attributes are valid" do
         it "updates the post" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:mentor_post, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: "New Title!")
           question.reload
 
@@ -165,8 +197,10 @@ RSpec.describe QuestionsController, type: :controller do
         end
 
         it "redirects them to the question url" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:mentor_post, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: "New Title!")
 
           expect(response).to redirect_to question_path(question.id)
@@ -175,8 +209,10 @@ RSpec.describe QuestionsController, type: :controller do
 
       context "when the attributes are invalid" do
         it "doesn't update the post" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:mentor_post, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: nil, content: "New content")
           question.reload
 
@@ -184,8 +220,10 @@ RSpec.describe QuestionsController, type: :controller do
         end
 
         it "renders the edit page" do
+          user = FactoryGirl.create(:user)
           question = FactoryGirl.create(:mentor_post, user_id: user.id)
 
+          sign_in user
           put :update, id: question.id, question: FactoryGirl.attributes_for(:question, title: nil, content: "New content")
 
           expect(response).to render_template :edit
@@ -195,9 +233,11 @@ RSpec.describe QuestionsController, type: :controller do
 
     context "when the user isn't person who created the post" do
       it "should redirect them to the question path" do
+        user = FactoryGirl.create(:user)
         another_user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: another_user.id)
 
+        sign_in user
         put :update, id: question.id, question:FactoryGirl.attributes_for(:question, content: "New content")
 
         expect(response).to redirect_to question_path(question)
@@ -206,22 +246,24 @@ RSpec.describe QuestionsController, type: :controller do
   end
 
   describe "DELETE #destroy" do
-    let(:user) { FactoryGirl.create(:user) }
-    before :each do
-      sign_in user
-    end
 
     context "when the user is person who created the post" do
       it "should delete the post" do
+        user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: user.id)
+
+        sign_in user
+
         expect{
           delete :destroy, id: question.id
         }.to change(Question, :count).by(-1)
       end
 
       it "should redirect the user to the original lesson url" do
+        user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: user.id)
 
+        sign_in user
         delete :destroy, id: question.id
 
         expect(response).to redirect_to "/html_css_lessons/1"
@@ -230,9 +272,11 @@ RSpec.describe QuestionsController, type: :controller do
 
     context "when the user isn't person who created the post" do
       it "should redirect them to the question path" do
+        user = FactoryGirl.create(:user)
         another_user = FactoryGirl.create(:user)
         question = FactoryGirl.create(:question, user_id: another_user.id)
 
+        sign_in user
         delete :destroy, id: question.id
 
         expect(response).to redirect_to question_path(question)
--- spec/controllers/student_sessions_controller_spec.rb
+++ spec/controllers/student_sessions_controller_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 RSpec.describe StudentSessionsController, type: :controller do
   let(:mentor) { FactoryGirl.create(:mentor) }
-  let(:user) { FactoryGirl.create(:user, mentor_id: mentor.id) }
-
-  before :each do
-    sign_in user
-  end
+  # let(:user) { FactoryGirl.create(:user, mentor_id: mentor.id) }
+  #
+  # before :each do
+  #   sign_in user
+  # end
 
   describe "GET #index" do
     context "when user is signed in" do
       it "should render index" do
+        user = FactoryGirl.create(:user, mentor_id: mentor.id)
+        sign_in user
+
         get :index
 
         expect(response).to render_template :index
@@ -20,6 +23,9 @@ RSpec.describe StudentSessionsController, type: :controller do
 
   describe "GET #new" do
     it "should render new template" do
+      user = FactoryGirl.create(:user, mentor_id: mentor.id)
+      sign_in user
+
       get :new
 
       expect(response).to render_template :new
@@ -29,6 +35,9 @@ RSpec.describe StudentSessionsController, type: :controller do
   describe "POST #create" do
     context "when attributes are valid" do
       it "should create a new mentor session" do
+        user = FactoryGirl.create(:user, mentor_id: mentor.id)
+        sign_in user
+
         mentor_session = FactoryGirl.create(:mentor_session, user_id: user.id, mentor_id: mentor.id)
 
         expect{
@@ -39,8 +48,11 @@ RSpec.describe StudentSessionsController, type: :controller do
 
     context "when attributes are invalid" do
       it "shouldn't create a new mentor session" do
+        user = FactoryGirl.create(:user, mentor_id: mentor.id)
+        sign_in user
+
         mentor_session = FactoryGirl.create(:mentor_session, user_id: user.id, mentor_id: mentor.id)
-        
+
         expect{
           post :create, student_session: FactoryGirl.attributes_for(:invalid_student_session, mentor_session_id: mentor_session.id)
         }.to change(StudentSession, :count).by(0)
@@ -48,3 +60,7 @@ RSpec.describe StudentSessionsController, type: :controller do
     end
   end
 end
+
+# def setup_user
+#
+# end
