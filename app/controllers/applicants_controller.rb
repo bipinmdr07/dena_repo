e306030debf5f7bea99b60f10309ffc5caefaeac
aadeshere <aadeshere1@gmail.com>
2016-10-03 15:45:39 +0545

    refactor tests

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -1,6 +1,6 @@
 class ApplicantsController < ApplicationController
   before_action :authenticate_user!
-  before_action :authenticate_admin!  
+  before_action :authenticate_admin!
 
   def index
     @applicants = User.unadmitted.order("created_at DESC")
