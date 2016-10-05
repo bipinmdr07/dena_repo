35560548850b88096e9a7e1004dc1627899307cf
Take Mouri <takehiromouri@gmail.com>
2016-10-05 14:35:58 +0545

    add question component

--- app/assets/javascripts/components/question.js.jsx
+++ app/assets/javascripts/components/question.js.jsx
@@ -1,9 +1,9 @@
-let Question = React.createClass({
+var Question = React.createClass({
   getInitialState() {
       return {
         question: this.props.question,
         user: this.props.user,
-        user_img_src: this.props.user_img_src,
+        user_img_src: this.props.user.avatar.url,
         display_post_links: this.props.display_edit_post_links,
         editing: false
       };
@@ -24,28 +24,39 @@ let Question = React.createClass({
     let delete_button = this.props.display_post_links ? <button className="btn btn-sm btn-danger" onClick={this.handleDelete}>Delete</button> : '';
     return (
       <div className="edit_post_links">
-        {edit_button}
-        {delete_button}
+        <div className="btn-group">
+          {edit_button}
+          {delete_button}
+        </div>
       </div>
     )
+  },
+
+  escapeHTML(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
   },
 
   render() {
 
 
     return(
-      <div class="row">
-        <div class="col-xs-12 col-sm-2">
+      <div className="row">
+        <div className="col-xs-12 col-sm-2">
           <img src={this.props.user_img_src} className="prof_pic_forum"/>
-          <div class="prof_name">
+          <div className="prof_name">
             {this.props.user.name}
           </div>
           {this.prof_badge()}
           {this.edit_post_links()}
         </div>
 
-        <div class="col-xs-12 col-sm-10">
-          
+        <div className="col-xs-12 col-sm-10">
+          <div dangerouslySetInnerHTML={{__html: this.props.question.content}} />
         </div>
       </div>
     )
