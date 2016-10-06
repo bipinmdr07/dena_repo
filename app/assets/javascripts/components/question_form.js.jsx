a3c2c418c1ddde45b62222f0873b521e4669d828
Take Mouri <takehiromouri@gmail.com>
2016-10-06 10:34:40 +0545

    add placeholder

--- app/assets/javascripts/components/question_form.js.jsx
+++ app/assets/javascripts/components/question_form.js.jsx
@@ -30,7 +30,13 @@ let QuestionForm = React.createClass({
         <div className="col-xs-12 col-md-6 col-md-offset-3"> 
           <form className="forum-forms">
             <input type='hidden' name='authenticity_token' value={this.props.authenticity_token} />
-            <textarea name="content" value={this.state.content} ref="content" className="form-control" onChange={this.handleChange} rows="5" />
+            <textarea name="content" 
+                      value={this.state.content} 
+                      ref="content" 
+                      className="form-control" 
+                      onChange={this.handleChange} 
+                      rows="5"
+                      placeholder="Write your reply in Markdown" />
             <button className="btn btn-cta-primary submit-btn" onClick={this.handleSubmit} disabled={!this.state.content}>Submit</button>
           </form>
         </div>
