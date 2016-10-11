56fe404f14b0a7724f8c1a4b3572e45b636b9c6a
Take Mouri <takehiromouri@gmail.com>
2016-10-11 09:39:29 +0545

    add config for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -89,4 +89,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://techrise-staging.herokuapp.com']
+
+  config.web_socket_server_url = "wss://techrise-staging.herokuapp.com/cable" 
+
 end
