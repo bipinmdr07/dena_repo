2f57d218751c95f7b7a88c6a1ed75b82067a1fe0
Take Mouri <takehiromouri@gmail.com>
2016-10-26 14:43:25 +0545

    update landing page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,11 +1,12 @@
 module StaticPagesHelper
   def skills
 		items = []
-		items << ['Ruby on Rails', "Learn how to create dynamic apps with Ruby on Rails.", 'Ruby_logo.png']
-		items << ['HTML/CSS', "Craft beautiful front-end designs with HTML and CSS.", 'HTML_CSS.svg.png']
-		items << ['JavaScript/jQuery', "Make your websites interactive with JavaScript and jQuery.", 'javascript.png']
-		items << ['Git/GitHub', "Learn what version control is and how you can use Git to collaborate with other awesome developers.", 'github.png']
-		items << ['Team Communication', "Pair up with another developer to work on a project together. We teach you how to use tools effectively for project management.", 'team.png']
+    items << ['Ruby', "Learn how to create dynamic apps with Ruby on Rails.", 'Ruby_logo.png']
+		items << ['Ruby on Rails', "Learn how to create dynamic apps with Ruby on Rails.", 'landing_page/ruby_on_rails.png']
+		items << ['HTML/CSS', "Craft beautiful front-end designs with HTML and CSS.", 'landing_page/html.png']
+		items << ['JavaScript', "Make your websites interactive with JavaScript and jQuery.", 'landing_page/javascript.png']
+		items << ['Git', "Learn what version control is and how you can use Git to collaborate with other awesome developers.", 'landing_page/github.png']
+		items << ['Teamwork', "Pair up with another developer to work on a project together. We teach you how to use tools effectively for project management.", 'landing_page/teamwork.png']
 		# items << ['Data Structures/Algorithms', "Learning data structures and algorithms are essential in your path to becoming a coder and will boost your logical thinking skills.", 'code.png']
   end
 end
