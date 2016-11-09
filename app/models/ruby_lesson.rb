5986e661b9707992eadc73257ba428653f8c950d
Take Mouri <takehiromouri@gmail.com>
2016-11-09 16:17:04 +0545

    revise lessons

--- app/models/ruby_lesson.rb
+++ app/models/ruby_lesson.rb
@@ -33,10 +33,11 @@ class RubyLesson < Lesson
     Lesson.new(15, "Instance Variables", chapter_2),
     Lesson.new(16, "attr_reader and attr_writer", chapter_2),
     Lesson.new(17, "Instance Methods, Class Methods, and Encapsulation", chapter_2),
-    Lesson.new(18, "Challenge: Building a Deck of Cards", chapter_2),
+    Lesson.new(18, "Inheritance and Composition", chapter_2),
     Lesson.new(19, "Foobar Challenge", chapter_3),
     Lesson.new(20, "Test-First Ruby: Hello", chapter_3),
-    Lesson.new(21, "Challenge: Temperature Conversion", chapter_3)
+    Lesson.new(21, "Challenge: Temperature Conversion", chapter_3),
+    Lesson.new(22, "Challenge: Building a Deck of Cards", chapter_3)
   ]
 
 
