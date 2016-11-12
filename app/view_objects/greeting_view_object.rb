a515b4e5a841f67aa29c968616dd0641545f60b3
Take Mouri <takehiromouri@gmail.com>
2016-11-12 22:59:04 +0545

    fix bug

--- app/view_objects/greeting_view_object.rb
+++ app/view_objects/greeting_view_object.rb
@@ -8,7 +8,7 @@ class GreetingViewObject
      return Greeting.new("Afternoon","dashboard/afternoon.png")
     elsif five_pm.upto(eight_pm).include?(current_time)
      return Greeting.new("Evening","dashboard/evening.png")
-    elsif eight_pm.upto(midnight  1.day).include?(current_time)
+    else      
      return Greeting.new("Night","dashboard/night.png")
     end
   end
