587c8cb9193f3f99e06f73e49b6c98f0f0d3995d
Take Mouri <takehiromouri@gmail.com>
2016-11-14 20:36:46 +0545

    fix lesson

--- app/assets/javascripts/components/admin_quiz_problems.js.jsx
+++ app/assets/javascripts/components/admin_quiz_problems.js.jsx
@@ -44,33 +44,57 @@ let AdminQuizProblems = React.createClass({
   },
 
   handleCourseChange(e){
-    let course_name = $(e.target).data('course-name');
-    
+    let course_name = $(e.target).data('course-name');    
+
     let quizProblems = this.state.quizProblems.filter((quizProblem) => {
       return quizProblem.course_name == course_name
     });
 
-
     this.setState({currentCourse: quizProblems});
   },
 
-  render(){
-    let quizCategories = this.state.quizCategories.map((o) => {
-      return <li>{o.name}</li>
+  handleChangeLesson(e){
+    let lessonId = $(e.target).data('lesson-id');    
+    var quizProblems = [];
+    
+    this.state.quizProblems.forEach((object) => {
+      object.quiz_problems.forEach((p) => {
+        console.log(p.lesson_id == lessonId)
+      })
+      object.quiz_problems.forEach((quizProblem) => {
+        if(quizProblem.lesson_id == lessonId){
+          quizProblems.push(quizProblem);
+        }
+      });
     });
 
+    this.setState({currentCourse: [quizProblems]});
+  },
+
+  render(){
     let quizProblemsHtml = [];
 
+    console.log(this.state.currentCourse)
+
     this.state.currentCourse.forEach((object) => {
-      quizProblemsHtml.push(<h1 key={object.course_name} className="text-center" style={{"clear": "both"}}>{object.course_name}</h1>);
-      object.quiz_problems.map((quiz_problem) => {
+      quizProblemsHtml.push(<h1 key={object.course_name} className="text-center" style={{"clear": "both"}}>{object.course_name}</h1>);      
+
+      object.quiz_problems.forEach((quiz_problem) => {
         quizProblemsHtml.push(<AdminQuizProblem key={quiz_problem.id} quizProblem={quiz_problem} handleDeleteQuiz={this.handleDeleteQuiz} quizCategories={this.state.quizCategories} handleCategorySubmit={this.handleCategorySubmit}/>);
       })
     });
 
-    let lessons = this.state.currentCourse.forEach((object) => {
-      return <li>Lesson {object.lesson_id}</li>
-    });
+    let lessonIds = [];
+
+    this.state.currentCourse.map((object) => {      
+      object.quiz_problems.map((quiz_problem) => {
+        lessonIds.push(quiz_problem.lesson_id)
+      });
+    });    
+
+    let lessons = _.uniq(lessonIds).map((id) => {
+      return <li><a onClick={this.handleChangeLesson} data-lesson-id={id}>Lesson {id}</a></li>
+    })
 
     return (
       <div>
