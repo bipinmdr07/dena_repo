13c701693e86ecef15c0fcdb6baffad367832907
Take Mouri <takehiromouri@gmail.com>
2016-11-19 22:33:20 +0545

    add algorithm course

--- app/models/algorithm_lesson.rb
+++ app/models/algorithm_lesson.rb
@@ -16,34 +16,17 @@ class AlgorithmLesson < Lesson
   ]
 
   LESSONS = [
-    Lesson.new(1, "Why test?", CHAPTERS[0]),
-    Lesson.new(2, "What is RSpec?", CHAPTERS[0]),
-
-    Lesson.new(3, "Setting up your Web Dev Environment", CHAPTERS[1]),
-    Lesson.new(4, "Setting Up Git", CHAPTERS[1]),
-    Lesson.new(5, "Self-Navigated Lesson: Installing Bootstrap", CHAPTERS[1]),
-    Lesson.new(6, "Self-Navigated Lesson: Installing Simple Form", CHAPTERS[1]),
-    Lesson.new(8, "Installing RSpec", CHAPTERS[1]),
-    Lesson.new(9, "Installing Factory Girl", CHAPTERS[1]),
-    Lesson.new(10, "Self Directed Lesson - Adding a Navbar", CHAPTERS[1]),
-
-    Lesson.new(11, "Creating the Posts Controller and Model", CHAPTERS[2]),
-    Lesson.new(12, "Setting Up Validations", CHAPTERS[2]),
-    Lesson.new(13, "Setting Up Routes", CHAPTERS[2]),
-    Lesson.new(14, "Setting up CRUD Actions", CHAPTERS[2]),
-
-    Lesson.new(11, "Creating the Posts Controller and Model", CHAPTERS[3]),
-    Lesson.new(12, "Setting Up Validations", CHAPTERS[3]),
-    Lesson.new(13, "Setting Up Routes", CHAPTERS[3]),
-    Lesson.new(14, "Setting up CRUD Actions", CHAPTERS[3]),
-
-    Lesson.new(15, "Why TDD?", CHAPTERS[4]),
-    Lesson.new(16, "Testing the index action", CHAPTERS[4]),
-    Lesson.new(17, "Creating New Posts", CHAPTERS[4]),
-    Lesson.new(18, "Individual Post Pages", CHAPTERS[4]),
-    Lesson.new(19, "Editing Posts", CHAPTERS[4]),
-    Lesson.new(20, "Deleting Posts", CHAPTERS[4]),
-
+    Lesson.new(1, "Introduction", CHAPTERS[0]),
+    Lesson.new(2, "Big-O Notation", CHAPTERS[0]),
+    Lesson.new(3, "Walking through a Problem", CHAPTERS[0]),
+    Lesson.new(4, "Rotating a Matrix", CHAPTERS[0]),
+    Lesson.new(4, "Create a Stack", CHAPTERS[0]),
+    Lesson.new(5, "Create a Queue", CHAPTERS[0]),
+    Lesson.new(4, "Reverse a Linked List", CHAPTERS[0]),
+    Lesson.new(5, "Detect a Cycle", CHAPTERS[0]),
+    Lesson.new(5, "Traversing a Binary Tree", CHAPTERS[0]),
+    Lesson.new(5, "Binary Search Tree", CHAPTERS[0]),
+    Lesson.new(5, "Sorting a Binary Tree", CHAPTERS[0]),
     Lesson.new(21, "Bubble Sort", CHAPTERS[5]),
     Lesson.new(22, "Selection Sort", CHAPTERS[5]),
     Lesson.new(23, "Insertion Sort", CHAPTERS[5]),
