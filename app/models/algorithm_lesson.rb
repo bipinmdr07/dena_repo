bdb45da227e66d59c1531d1bc56f00449e7374b0
Take Mouri <takehiromouri@gmail.com>
2016-11-20 23:33:37 +0545

    save lessons

--- app/models/algorithm_lesson.rb
+++ app/models/algorithm_lesson.rb
@@ -8,29 +8,29 @@ class AlgorithmLesson < Lesson
 
   CHAPTERS = [
     Chapter.new("Introduction to Data Structures and Algorithms"),
-    Chapter.new("Arrays and Linked Lists"),
+    Chapter.new("Big-O Time Complexity"),
+    Chapter.new("Arrays"),
     Chapter.new("Stacks and Queues"),
-    Chapter.new("Hashes"),
+    Chapter.new("Arrays and Linked Lists"),    
     Chapter.new("Trees"),
     Chapter.new("Sorting Algorithms")
   ]
 
   LESSONS = [
     Lesson.new(1, "Introduction", CHAPTERS[0]),
-    Lesson.new(2, "Big-O Notation", CHAPTERS[0]),
-    Lesson.new(3, "Walking through a Problem", CHAPTERS[0]),
-    Lesson.new(4, "Rotating a Matrix", CHAPTERS[0]),
-    Lesson.new(4, "Create a Stack", CHAPTERS[0]),
-    Lesson.new(5, "Create a Queue", CHAPTERS[0]),
-    Lesson.new(4, "Reverse a Linked List", CHAPTERS[0]),
-    Lesson.new(5, "Detect a Cycle", CHAPTERS[0]),
-    Lesson.new(5, "Traversing a Binary Tree", CHAPTERS[0]),
-    Lesson.new(5, "Binary Search Tree", CHAPTERS[0]),
-    Lesson.new(5, "Sorting a Binary Tree", CHAPTERS[0]),
-    Lesson.new(21, "Bubble Sort", CHAPTERS[5]),
-    Lesson.new(22, "Selection Sort", CHAPTERS[5]),
-    Lesson.new(23, "Insertion Sort", CHAPTERS[5]),
-    Lesson.new(24, "Quick Sort", CHAPTERS[5]),
-    Lesson.new(25, "Merge Sort", CHAPTERS[5])
+    Lesson.new(2, "Big-O Notation", CHAPTERS[1]),
+    Lesson.new(3, "Analyzing the Time Complexity", CHAPTERS[1]),
+    Lesson.new(4, "Walking through a Problem", CHAPTERS[1]),
+    Lesson.new(5, "Rotating a Matrix", CHAPTERS[2]),
+    Lesson.new(6, "Create a Stack", CHAPTERS[3]),
+    Lesson.new(7, "Create a Queue", CHAPTERS[3]),
+    Lesson.new(8, "Reversing a Linked List and Cycle Detection", CHAPTERS[4]),
+    Lesson.new(9, "Traversing a Binary Tree", CHAPTERS[5]),
+    Lesson.new(12, "Sorting a Binary Tree", CHAPTERS[5]),
+    Lesson.new(13, "Bubble Sort", CHAPTERS[6]),
+    Lesson.new(14, "Selection Sort", CHAPTERS[6]),
+    Lesson.new(15, "Insertion Sort", CHAPTERS[6]),
+    Lesson.new(16, "Quick Sort", CHAPTERS[6]),
+    Lesson.new(17, "Merge Sort", CHAPTERS[6])
   ]
 end
