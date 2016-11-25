2389b9632fdc332bfdf7fc0d1a9767f7a65db0c3
Takehiro <takehiromouri@gmail.com>
2016-11-25 11:31:02 +0545

    update migrations

--- db/migrate/20151219205123_change_comments_and_forums.rb
+++ db/migrate/20151219205123_change_comments_and_forums.rb
@@ -1,5 +1,6 @@
 class ChangeCommentsAndForums < ActiveRecord::Migration
   def change
+  	return unless table_exists? :forums
   	remove_column :forums, :chapter
   	remove_column :forums, :lesson
   	add_column :forums, :name, :string
--- db/migrate/20160706074648_change_column_name_to_resolved.rb
+++ db/migrate/20160706074648_change_column_name_to_resolved.rb
@@ -1,5 +1,6 @@
 class ChangeColumnNameToResolved < ActiveRecord::Migration
   def change
+  	return unless table_exists? :comments
     rename_column :comments, :solved, :resolved
   end
 end
