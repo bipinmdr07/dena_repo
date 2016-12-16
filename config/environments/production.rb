4c3d60ea364e17ef76d73dfdf4ac9fe4e582147c
Takehiro Mouri <takehiromouri@gmail.com>
2016-12-16 13:34:53 +0900

    fix bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   ActionMailer::Base.smtp_settings = {
-    :address => ‘smtp.sendgrid.net’,
+    :address => 'smtp.sendgrid.net',
     :port => ‘587’,
     :authentication => :plain,
     :user_name => ENV['SENDGRID_USERNAME'],
